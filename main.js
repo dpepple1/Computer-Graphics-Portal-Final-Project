d0fc824f1953cc783247a650aab3ba9a36052ece
epineda2 <epineda2@nd.edu>
2023-04-13 01:50:01 -0400

    Camera changes

--- main.js
+++ main.js
@@ -1,25 +1,96 @@
+/*****************************/
+// Filename: main.js
+// Project: Portal
+// Group: Eduardo P., Derek P., Steven G.
+/*****************************/
 
+/* Libraries */
 import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+/* Variables */
+// Note: Using 'let' for strict type-checking
+let container, stats; // holds data
+let camera, controller; // camera
+let scene, renderer;  // rendered scene
+let geometry, cube, material;   // testing cube
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+/* Constant World Values */
+const clock = new THREE.Clock();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+/* Global Parameters (for easy access) */
+let sceneColor = 0xefd1b5;
 
-camera.position.z = 5;
+initialize();
+animate();
 
+function initialize() {
+    // Container holding canvas
+    container = document.getElementById("container");
+
+    // Scene creation
+    init_scene(); 
+
+    // Renderer activation
+    init_renderer();
+
+    // Camera activation
+    init_camera();
+
+    // Stats (optional)
+    init_stats();
+
+    // Create initial scene
+    init_geometry();
+}
+
+function init_scene() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(sceneColor);
+}
+function init_geometry() {
+    geometry = new THREE.BoxGeometry(100, 100, 10);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
+
+function init_stats() {
+    stats = new Stats();
+    container.appendChild(stats.dom);
+}
+
+/* Establishes Renderer */
+function init_renderer() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+}
+
+/* Establishes Controllable Camera Object */
+function init_camera() {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1,10000);
+    controller = new FirstPersonControls(camera, renderer.domElement);
+    controller.lookSpeed = 0.8;
+    controller.movementSpeed = 5;
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    controller.handleResize();
+}
 
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    render();
+    stats.update();
 }
-animate();
+
+function render() {
+    controller.update();
+    renderer.render(scene, camera);
+}
